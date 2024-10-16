e60c58984f8f8c72f73f6e73a755062e81532605
heitorcolangelo <heitor.colangelo@getdrop.com>
2024-10-16 12:59:57 +0100

    Revert "test: Fail to check if PR is blocked"
    This reverts commit e447943c4ea6ce9a0137db24d528c7710f33da9e.

--- core/src/commonTest/kotlin/com/kmpkit/SampleTest.kt
+++ core/src/commonTest/kotlin/com/kmpkit/SampleTest.kt
@@ -2,12 +2,10 @@ package com.kmpkit
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.fail
 
 class SampleTest {
     @Test
     fun sampleTest() {
-        fail()
         assertEquals(expected = true, actual = true)
     }
 }

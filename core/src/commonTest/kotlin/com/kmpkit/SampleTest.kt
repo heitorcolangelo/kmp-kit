e447943c4ea6ce9a0137db24d528c7710f33da9e
heitorcolangelo <heitor.colangelo@getdrop.com>
2024-10-16 12:34:18 +0100

    test: Fail to check if PR is blocked

--- core/src/commonTest/kotlin/com/kmpkit/SampleTest.kt
+++ core/src/commonTest/kotlin/com/kmpkit/SampleTest.kt
@@ -2,10 +2,12 @@ package com.kmpkit
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class SampleTest {
     @Test
     fun sampleTest() {
+        fail()
         assertEquals(expected = true, actual = true)
     }
 }

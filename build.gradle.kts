8eb17f4939368c32fd709d007b771f65776497f7
heitorcolangelo <heitor.colangelo@getdrop.com>
2024-10-19 15:55:07 +0100

    fix: URL for github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    `maven-publish`
 }
 
 subprojects {
     apply(plugin = "maven-publish")
 
-    group = "com.heitorcolangelo.kmpkit"
     version = this.property("version") as String? ?: "0.0.0"
 
     configure<PublishingExtension> {
         repositories {
             maven {
                 name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/heitorcolangelo/KMPKit")
+                url = uri("https://maven.pkg.github.com/heitorcolangelo/kmp-kit")
                 credentials {
                     username = System.getenv("GITHUB_ACTOR")
                     password = System.getenv("GITHUB_TOKEN")
